dfd50dbf0bf03f856cbf12eb07166aaa2d997c9d
mathon_j <mathon_j@epitech.eu>
2015-11-29 18:23:56 +0100

    up weather

--- inde/time.js
+++ inde/time.js
@@ -37,5 +37,24 @@ function initializeClock(id, endtime){
   var timeinterval = setInterval(updateClock,1000);
 }
 
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'New Dehli, IND',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
+
 var deadline = 'August 01 2016 00:00:01 UTC+0200';
 initializeClock('clockdiv', deadline)
