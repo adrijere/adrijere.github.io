d3a98ab1121a12cc65a85755fe2d872c03ff974b
mathon_j <mathon_j@epitech.eu>
2015-11-29 16:26:03 +0100

    map with rot.js

--- roguelike.js
+++ roguelike.js
@@ -1,36 +1,46 @@
-window.onload = function()
-{
-    var canvas = document.getElementById('map');
-    if(!canvas)
-    {
-        alert("Impossible de récupérer le canvas");
-        return;
-    }
+var Game = {
+    display: null,
+    map: {},
     
-    var context = canvas.getContext('2d');
-    if(!context)
-    {
-        alert("Impossible de récupérer le context du canvas");
-        return;
+    init: function() {
+        this.display = new ROT.Display();
+        document.body.appendChild(this.display.getContainer());
+        
+        this._generateMap();
+    },
+    
+    _generateMap: function() {
+        var digger = new ROT.Map.Digger();
+        var freeCells = [];
+        
+        var digCallback = function(x, y, value) {
+            if (value) { return; }
+            
+            var key = x+","+y;
+            this.map[key] = ".";
+            freeCells.push(key);
+        }
+        digger.create(digCallback.bind(this));
+        
+        this._generateBoxes(freeCells);
+        
+        this._drawWholeMap();
+    },
+    
+    _generateBoxes: function(freeCells) {
+        for (var i=0;i<10;i++) {
+            var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+            var key = freeCells.splice(index, 1)[0];
+            this.map[key] = "*";
+        }
+    },
+    
+    _drawWholeMap: function() {
+        for (var key in this.map) {
+            var parts = key.split(",");
+            var x = parseInt(parts[0]);
+            var y = parseInt(parts[1]);
+            this.display.draw(x, y, this.map[key]);
+        }
     }
-}
-
-var roguelike = new function() {
-    this.player;
-    this.map;
-    this.width;
-    this.height;
-    this.create_map = function(x, y, color) {
-     var canvas = document.getElementById('map');
-        canvas.width = x;
-        canvas.height = y;
-        canvas.style.backgroundColor = color || "green";
-    };
-    this.create_player = function(x, y, sprite) {
-    document.getElementById("map").innerHTML = "<div id=\"player\"></div>";
-    var player = document.getElementById('player');
-    player.style.width = x;
-    player.style.height = y;
-    player.style.backgroundColor = "red";
-    };
-}
+};
